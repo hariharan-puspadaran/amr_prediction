33f2093df540c718203fbf5d6ceb0a5e66ac4443
Hariharan Puspadaran <hariharanpuspadaran@gmail.com>
2023-01-15 20:18:39 +0800

    Update errors

--- main_page.py
+++ main_page.py
@@ -4,7 +4,13 @@ import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
 
 st.title ("AMR Prediction using Genotypic Data")
-st.header("Klebsiella pneumoniae")
+
+if 'data' not in st.session_state:
+    st.session_state['data'] = pd.DataFrame()
+#Section for users to choose the type of microbe to evaluate
+option =""
+option = st.selectbox('Microbe Species: ',('Klebsiella pneumoniae', 'Acinetobacter baumannii', 'Pseudomonas aeruginosa')) 
+st.header(option)
 with st.container():
     st.markdown("Below is a template of the data spreadsheet")
     df = pd.read_csv("CRKP - Template.csv")
@@ -18,27 +24,65 @@ with st.container():
 
 st.header("Upload the spreadsheet with data to be classified")
 
-import streamlit as st
-import os.path
-import pathlib
+def check_csv(df):
+    if {'NDM','VIM','OXA','KPC','IMP','TEM','SHV','AmpC','CTX-M-1'}.issubset(df.columns):
+        return True
+    else:
+        return False
+    
+def geterrors(dfObj, value):
+    col_list = []
+    row_list = []
+    results = dfObj.isin([value])
+    check_list = results.any()
+    col_names = list(check_list[check_list == True].index)
+    
+    for here in col_names:
+        rows = list(results[here][results[here] == True].index)
+        for row in rows:
+            col_list.append(here)
+            row_list.append(dfObj._get_value(row, 'Strain label'))
+            
+    return col_list, row_list
 
 uploaded_file = st.file_uploader("Choose a CSV file",type=['csv'],)
 if uploaded_file is not None:
     new_df = pd.read_csv(uploaded_file)
-    prev_df = new_df.head(5)
-    st.markdown("Preview of uploaded csv file")
-    st.dataframe(prev_df)
+    checker = check_csv(new_df)
+    if checker:
+        val_holder = new_df.loc[:, new_df.columns != 'Strain label'].values
+        temp = val_holder[val_holder != '+']
+        answer = temp[temp != '-']
+        if answer.any():
+            unique_err = list(set(answer))
+            checker = False
+            for i in range(len(unique_err)):
+                column, row = geterrors(new_df, unique_err[i])
+                for j in range(len(row)):
+                    output_warning = "Invalid value at Strain Label: " + str(row[j]) + " and Column: " + str(column[j])
+                    st.markdown(output_warning)
+        if checker:         
+            prev_df = new_df.head(5)
+            st.markdown("Preview of uploaded csv file")
+            st.dataframe(prev_df)
+            
+        if checker:
+            submit = st.button("Submit")
+            if submit:
+                st.session_state['data'] = new_df
+                switch_page("results")
+    else:
+        st.markdown("Invalid File. Please use the template file to record the data")
 
-from streamlit_extras.switch_page_button import switch_page
+# if 'data' not in st.session_state:
+#     st.session_state['data'] = pd.DataFrame()
+# if checker:
+#     submit = st.button("Submit")
+#     if submit:
+#         st.session_state['data'] = new_df
+#         switch_page("results")
 
-if 'data' not in st.session_state:
-    st.session_state['data'] = pd.DataFrame()
     
-submit = st.button("Submit")
-if submit:
-    st.session_state['data'] = new_df
-    switch_page("results")
-
 # st.caption("this is the caption")
 # st.code("x=2021")
 # st.latex(r''' a+a r^1+a r^2+a r^3 ''')
--- pages/results.py
+++ pages/results.py
@@ -7,43 +7,45 @@ from datetime import date
 st.title ("AMR Prediction using Genotypic Data")
 st.header("Klebsiella pneumoniae")
 
-x_final = st.session_state['data']
-x_val = st.session_state['data'].replace(['-'],'0').replace(['+'],'1')
-x_val = x_val.iloc[:,1:10].values
-x_test = x_val.astype('int')
+if st.session_state['data'].empty:
+    st.markdown("Please upload a dataset from the main page")
+else:
+     
+    x_final = st.session_state['data']
+    x_val = st.session_state['data'].replace(['-'],'0').replace(['+'],'1')
+    x_val = x_val.iloc[:,1:10].values
+    x_test = x_val.astype('int')
 
 
-#IMIPENEM
-loaded_model = pickle.load(open('IMIPENEM.sav', 'rb'))
-result_imi = loaded_model.predict(x_test).astype('int')
-x_final['IMIPENEM'] = result_imi.tolist()
+    #IMIPENEM
+    loaded_model = pickle.load(open('IMIPENEM.sav', 'rb'))
+    result_imi = loaded_model.predict(x_test).astype('int')
+    x_final['IMIPENEM'] = result_imi.tolist()
 
-#MEROPENEM
-loaded_model = pickle.load(open('MEROPENEM.sav', 'rb'))
-result_mero = loaded_model.predict(x_test)
-x_final['MEROPENEM'] = result_mero.tolist()
+    #MEROPENEM
+    loaded_model = pickle.load(open('MEROPENEM.sav', 'rb'))
+    result_mero = loaded_model.predict(x_test)
+    x_final['MEROPENEM'] = result_mero.tolist()
 
-#DORIPENEM
-loaded_model = pickle.load(open('DORIPENEM.sav', 'rb'))
-result_dori = loaded_model.predict(x_test)
-x_final['DORIPENEM'] = result_dori.tolist()
+    #DORIPENEM
+    loaded_model = pickle.load(open('DORIPENEM.sav', 'rb'))
+    result_dori = loaded_model.predict(x_test)
+    x_final['DORIPENEM'] = result_dori.tolist()
 
-#CEFIDEROCOL
-loaded_model = pickle.load(open('CEFIDEROCOL.sav', 'rb'))
-result_cefi= loaded_model.predict(x_test)
-x_final['CEFIDEROCOL'] = result_cefi.tolist()
+    #CEFIDEROCOL
+    loaded_model = pickle.load(open('CEFIDEROCOL.sav', 'rb'))
+    result_cefi= loaded_model.predict(x_test)
+    x_final['CEFIDEROCOL'] = result_cefi.tolist()
 
-x_final = x_final.replace([0],'S').replace([1],'I').replace([2],'R')
-# st.dataframe(x_final)
+    x_final = x_final.replace([0],'S').replace([1],'I').replace([2],'R')
+    # st.dataframe(x_final)
+    st.dataframe(x_final)
+    result_csv = x_final.to_csv(index=False).encode('utf-8')
 
-
-st.dataframe(x_final)
-result_csv = x_final.to_csv(index=False).encode('utf-8')
-
-with st.container():
-    st.download_button(
-    label = "📥 Download result as .csv",
-    data = result_csv,
-    file_name = 'CRKP - Result '+date.today().strftime("%d.%m.%Y")+'.csv',
-    mime='text/csv',
+    with st.container():
+        st.download_button(
+        label = "📥 Download result as .csv",
+        data = result_csv,
+        file_name = 'CRKP - Result '+date.today().strftime("%d.%m.%Y")+'.csv',
+        mime='text/csv',
     )
