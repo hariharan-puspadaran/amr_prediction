9ea331260c53603a92f524d503c9187c70d73b01
Hariharan Puspadaran <hariharanpuspadaran@gmail.com>
2023-01-25 21:08:30 +0800

    Update errors

--- main_page.py
+++ main_page.py
@@ -7,28 +7,43 @@ st.title ("AMR Prediction using Genotypic Data")
 
 if 'data' not in st.session_state:
     st.session_state['data'] = pd.DataFrame()
+if 'model_type' not in st.session_state:
+    st.session_state['model_type'] = ""
 #Section for users to choose the type of microbe to evaluate
 option =""
 option = st.selectbox('Microbe Species: ',('Klebsiella pneumoniae', 'Acinetobacter baumannii', 'Pseudomonas aeruginosa')) 
 st.header(option)
 with st.container():
-    st.markdown("Below is a template of the data spreadsheet")
-    df = pd.read_csv("CRKP - Template.csv")
+    st.markdown("Please use the csv file below as a baseline structure to tabulate your data. The columnns represent the list of genes to be tested in this species.")
+    template_name = str(option)+" - Template.csv"
+    df = pd.read_csv(template_name)
     template_csv = df.to_csv(index=False).encode('utf-8')
     st.download_button(
     label = "📥 Press to Download Template",
     data = template_csv,
-    file_name = 'CRKP - Template.csv',
+    file_name = template_name,
     mime='text/csv',
     )
 
 st.header("Upload the spreadsheet with data to be classified")
 
-def check_csv(df):
-    if {'NDM','VIM','OXA','KPC','IMP','TEM','SHV','AmpC','CTX-M-1'}.issubset(df.columns):
-        return True
+def check_csv(df, option):
+    if (option == 'Klebsiella pneumoniae'):
+        if {'NDM','VIM','OXA','KPC','IMP','TEM','SHV','AmpC','CTX-M-1'}.issubset(df.columns):
+            return True
+        else:
+            return False
+    elif(option == 'Acinetobacter baumannii'):
+        if {'CTXM 1','CTXM 2','CTXM 9','GES','IMP','NDM','Oxa 23','Oxa 24','Oxa 51','Oxa 58','PER','SHV','SIM1','TEM','VEB','VIM'}.issubset(df.columns):
+            return True
+        else:
+            return False
     else:
-        return False
+        if {'ampC','IMP','VIM'}.issubset(df.columns):
+            return True
+        else:
+            return False
+        
     
 def geterrors(dfObj, value):
     col_list = []
@@ -48,7 +63,7 @@ def geterrors(dfObj, value):
 uploaded_file = st.file_uploader("Choose a CSV file",type=['csv'],)
 if uploaded_file is not None:
     new_df = pd.read_csv(uploaded_file)
-    checker = check_csv(new_df)
+    checker = check_csv(new_df,option)
     if checker:
         val_holder = new_df.loc[:, new_df.columns != 'Strain label'].values
         temp = val_holder[val_holder != '+']
@@ -70,9 +85,10 @@ if uploaded_file is not None:
             submit = st.button("Submit")
             if submit:
                 st.session_state['data'] = new_df
+                st.session_state['model_type'] = option
                 switch_page("results")
     else:
-        st.markdown("Invalid File. Please use the template file to record the data")
+        st.markdown("Invalid File. Please make sure the corect template file was used to record the data")
 
 # if 'data' not in st.session_state:
 #     st.session_state['data'] = pd.DataFrame()
--- pages/results.py
+++ pages/results.py
@@ -3,44 +3,126 @@ import pickle
 import sklearn
 import pandas as pd
 from datetime import date
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+
 
 st.title ("AMR Prediction using Genotypic Data")
-st.header("Klebsiella pneumoniae")
+if st.session_state['model_type']=="":
+    st.markdown("Please select a microbe type from the main page")
+else:
+    st.header(st.session_state['model_type'])
 
 if st.session_state['data'].empty:
     st.markdown("Please upload a dataset from the main page")
 else:
+    
+    option = st.session_state['model_type']
+    if (option == 'Klebsiella pneumoniae'):
+        x_final = st.session_state['data']
+        x_val = st.session_state['data'].replace(['-'],'0').replace(['+'],'1')
+        x_val = x_val.iloc[:,1:10].values
+        x_test = x_val.astype('int')
+        columns = ['IMIPENEM','MEROPENEM','DORIPENEM','CEFIDEROCOL']
+
+        #IMIPENEM
+        loaded_model = pickle.load(open('IMIPENEM.sav', 'rb'))
+        result_imi = loaded_model.predict(x_test).astype('int')
+        x_final['IMIPENEM'] = result_imi.tolist()
+
+        #MEROPENEM
+        loaded_model = pickle.load(open('MEROPENEM.sav', 'rb'))
+        result_mero = loaded_model.predict(x_test)
+        x_final['MEROPENEM'] = result_mero.tolist()
+
+        #DORIPENEM
+        loaded_model = pickle.load(open('DORIPENEM.sav', 'rb'))
+        result_dori = loaded_model.predict(x_test)
+        x_final['DORIPENEM'] = result_dori.tolist()
+
+        #CEFIDEROCOL
+        loaded_model = pickle.load(open('CEFIDEROCOL.sav', 'rb'))
+        result_cefi= loaded_model.predict(x_test)
+        x_final['CEFIDEROCOL'] = result_cefi.tolist()
+
+        x_final = x_final.replace([0],'S').replace([1],'I').replace([2],'R')
+
+    
+    elif (option == 'Acinetobacter baumannii'):
+        x_final = st.session_state['data']
+        x_val = st.session_state['data'].replace(['-'],'0').replace(['+'],'1')
+        x_val = x_val.iloc[:,1:16].values
+        x_test = x_val.astype('int')
+
+
+        #IMIPENEM
+        loaded_model = pickle.load(open('CRO_IMIPENEM.sav', 'rb'))
+        result_imi = loaded_model.predict(x_test).astype('int')
+        x_final['IMIPENEM'] = result_imi.tolist()
+
+        #MEROPENEM
+        loaded_model = pickle.load(open('CRO_MEROPENEM.sav', 'rb'))
+        result_mero = loaded_model.predict(x_test)
+        x_final['MEROPENEM'] = result_mero.tolist()
+
+        #DORIPENEM
+        loaded_model = pickle.load(open('CRO_DORIPENEM.sav', 'rb'))
+        result_dori = loaded_model.predict(x_test)
+        x_final['DORIPENEM'] = result_dori.tolist()
+
+        x_final = x_final.replace([0],'S').replace([1],'I').replace([2],'R')
+
+    else:
+        x_final = st.session_state['data']
+        x_val = st.session_state['data'].replace(['-'],'0').replace(['+'],'1')
+        x_val = x_val.iloc[:,1:4].values
+        x_test = x_val.astype('int')
+
+
+        #IMIPENEM
+        loaded_model = pickle.load(open('CRPA_IMIPENEM.sav', 'rb'))
+        result_imi = loaded_model.predict(x_test).astype('int')
+        x_final['IMIPENEM'] = result_imi.tolist()
+
+        #MEROPENEM
+        loaded_model = pickle.load(open('CRPA_MEROPENEM.sav', 'rb'))
+        result_mero = loaded_model.predict(x_test)
+        x_final['MEROPENEM'] = result_mero.tolist()
+
+        #DORIPENEM
+        loaded_model = pickle.load(open('CRPA_DORIPENEM.sav', 'rb'))
+        result_dori = loaded_model.predict(x_test)
+        x_final['DORIPENEM'] = result_dori.tolist()
+
+        x_final = x_final.replace([0],'S').replace([1],'I').replace([2],'R')
      
-    x_final = st.session_state['data']
-    x_val = st.session_state['data'].replace(['-'],'0').replace(['+'],'1')
-    x_val = x_val.iloc[:,1:10].values
-    x_test = x_val.astype('int')
 
+    tab1, tab2 = st.tabs(["Table", "Graph"])
+    with tab1: 
+        tab1.dataframe(x_final)
+        
+    with tab2:
+        visual_output = pd.DataFrame (columns, columns = ['Antimicrobial'])
+        
+        out_results = np.arange(len(columns) * 3).reshape(3, len(columns))
+        for i in range (len(columns)):
+            for j in range (3):
+                try:
+                    current = x_final[columns[i]].value_counts()[j].astype(int)
+                except IndexError:
+                    current = 0     
+                out_results[j][i] = current
+                
+        visual_output['Susceptible'] = out_results[0].astype(int)
+        visual_output['Intermediate'] = out_results[1].astype(int)
+        visual_output['Resistant'] = out_results[2].astype(int)
+        
+        
+        fig = px.bar(visual_output, x = 'Antimicrobial', y=['Susceptible','Intermediate','Resistant'])
+        tab2.plotly_chart(fig, theme="streamlit", use_conatiner_width=True)
 
-    #IMIPENEM
-    loaded_model = pickle.load(open('IMIPENEM.sav', 'rb'))
-    result_imi = loaded_model.predict(x_test).astype('int')
-    x_final['IMIPENEM'] = result_imi.tolist()
-
-    #MEROPENEM
-    loaded_model = pickle.load(open('MEROPENEM.sav', 'rb'))
-    result_mero = loaded_model.predict(x_test)
-    x_final['MEROPENEM'] = result_mero.tolist()
-
-    #DORIPENEM
-    loaded_model = pickle.load(open('DORIPENEM.sav', 'rb'))
-    result_dori = loaded_model.predict(x_test)
-    x_final['DORIPENEM'] = result_dori.tolist()
-
-    #CEFIDEROCOL
-    loaded_model = pickle.load(open('CEFIDEROCOL.sav', 'rb'))
-    result_cefi= loaded_model.predict(x_test)
-    x_final['CEFIDEROCOL'] = result_cefi.tolist()
-
-    x_final = x_final.replace([0],'S').replace([1],'I').replace([2],'R')
-    # st.dataframe(x_final)
-    st.dataframe(x_final)
-    result_csv = x_final.to_csv(index=False).encode('utf-8')
+    result_csv = x_final.to_csv(index=False).encode('utf-8')   
 
     with st.container():
         st.download_button(
